57dd42c8ee6e9cc98fbf7e7b751203df3903b05f
Juliano Palacios Abrantes <carmelia@Julianos-MacBook-Pro.local>
2020-04-24 11:54:02 -0300

    Created first three functions

---
+++ R/my_ggtheme_p.R
@@ -0,0 +1,30 @@
+#' My personal plot theme
+#'
+#' This function creates my own ggplot theme to 
+#' standarize multiple plots in one document
+#'
+#' @return a ggplot theme
+#' @export
+
+my_ggtheme_p <- function() {
+  theme(
+    plot.title = element_text(size = rel(1), hjust = 0, face = "bold"),
+    panel.background = element_blank(),
+    strip.background = element_blank(),
+    panel.border   = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_blank(),
+    axis.ticks = element_blank(),
+    axis.text.x = element_text(size = 14,
+                               angle = 0,
+                               face = "plain"),
+    axis.text.y = element_text(size = 14),
+    axis.title = element_text(size = 14),
+    legend.key = element_rect(colour = NA, fill = NA),
+    legend.position = "top",
+    legend.title = element_text(size = 16),
+    legend.text = element_text(size = 14),
+    strip.text.x = element_text(size = 10, colour = "black"),
+    strip.text = element_text(size = 18)
+  )
+}
