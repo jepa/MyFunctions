3ffa9d40289ef7ab582d5bf01250c421e1cc85b8
Juliano Palacios Abrantes <j.palacios@oceans.ubc.ca>
2021-03-09 16:31:38 -0300

    Update my_ggtheme_p.R
    Made some internal modifications to call on function

--- R/my_ggtheme_p.R
+++ R/my_ggtheme_p.R
@@ -6,7 +6,7 @@
 #' @return a ggplot theme
 #' @export
 
-my_ggtheme_p <- function() {
+my_ggtheme_p <- function(ax_tx_s = 14, ax_tl_s = 15, leg_pos = "top", leg_tl_s = 16, leg_tx = 14) {
   theme(
     plot.title = element_text(size = rel(1), hjust = 0, face = "bold"),
     panel.background = element_blank(),
@@ -14,16 +14,19 @@ my_ggtheme_p <- function() {
     panel.border   = element_blank(),
     panel.grid.minor = element_blank(),
     panel.grid.major = element_blank(),
+    axis.line = element_line(color = "black"),
     axis.ticks = element_blank(),
-    axis.text.x = element_text(size = 14,
+    axis.text.x = element_text(size = ax_tx_s,
                                angle = 0,
-                               face = "plain"),
-    axis.text.y = element_text(size = 14),
-    axis.title = element_text(size = 14),
+                               face = "plain",
+                              color = "black"),
+    axis.text.y = element_text(size = ax_tx_s,
+                              color = "black"),
+    axis.title = element_text(size = ax_tl_s),
     legend.key = element_rect(colour = NA, fill = NA),
-    legend.position = "top",
-    legend.title = element_text(size = 16),
-    legend.text = element_text(size = 14),
+    legend.position = leg_pos,
+    legend.title = element_text(size = legend_title_size),
+    legend.text = element_text(size = leg_tx),
     strip.text.x = element_text(size = 10, colour = "black"),
     strip.text = element_text(size = 18)
   )
