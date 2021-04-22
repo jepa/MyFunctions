d656d0c14fcd30da845368bd39fccdeb018dc2ff
Juliano Palacios Abrantes <j.palacios@oceans.ubc.ca>
2021-04-22 12:28:05 -0300

    Now includes direct paths to general Species and Spatial data

--- R/my_paths.R
+++ R/my_paths.R
@@ -8,7 +8,7 @@
 #' @param project I normally call my data folder the same as the project so it automatically sets to the project name. Alternatives available.
 #' @param extra_path Any sub-path to add WITHIN the folders. Note, if it is before, include it on the "Folder" parameter -e.g. project/SubFolder -
 #' @param name Dataset name in case you want the option to load data
-#' @param repo Normally set to jepa88 but if connection to Drobo is availbale use repo = "FALSE"
+#' @param repo Normally set to jepa88 but if connection to Drobo is available use repo = "FALSE"
 #' @param read Default read = FALSE. read = FALSE will only return a path wile read = TURE will read a dataset
 #' @return Paths to save and load data within a project
 #' @export
@@ -44,12 +44,18 @@ my_path <- function(path_type, extra_path= "", name = "", repo = TRUE, read=FALS
   if(path_type%in% c("G","Generic","g","generic")){
     Path <- Main_Path
   }
+  if(path_type%in% c("Spa","Spatial","spa","spatial")){
+    Path <- paste(Main_Path,"/Spatial",sep="")
+  }
+  if(path_type%in% c("Spp","Species","spp","species")){
+    Path <- paste(Main_Path,"/Species",sep="")
+  }
   if(path_type%in% c("F","f","Figures","Figure","figure","figures")){
     Path <- "./Figures"
   }
 
   # Error messages for misspelled variables
-  Options = c("R","Results","D","Data","G","Generic","F","Figures")
+  Options = c("R","Results","D","Data","G","Generic","F","Figures","Spa","Spatial","Spp","Species")
   if(!path_type %in% Options){
     print("Data type value not accepted. Current acceptable options:")
     print(Options)
@@ -66,8 +72,16 @@ my_path <- function(path_type, extra_path= "", name = "", repo = TRUE, read=FALS
     }else{
       My_Path <- data.table::fread(My_Path, header=header) %>% janitor::clean_names()
     }
+    
+    # Already load the coords with names
+    if(name == "Lon_Lat_DBEM.txt"){
+      
+      colnames(My_Path) <- c("index","lon","lat")
+    }
+    
+    
   }else{
-    My_Path <- paste(Path,extra_path,"",sep="/")
+    My_Path <- paste(Path,extra_path,name,"",sep="/")
     # Fix any double // in the path
     My_Path<- gsub("//","/",My_Path) 
   }
@@ -76,3 +90,7 @@ my_path <- function(path_type, extra_path= "", name = "", repo = TRUE, read=FALS
   
   return(My_Path)
 }
+
+# Test me
+# my_path("G", "Spatial/DBEM", "Lon_Lat_DBEM.txt", read = T)
+
