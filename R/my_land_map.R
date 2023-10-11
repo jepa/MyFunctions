bdc7c12b3c222a6590e1869bb54a14f115fa0bd5
Juliano Palacios Abrantes <j.palacios@oceans.ubc.ca>
2023-10-11 11:57:01 -0700

    Included automatic colors

--- R/my_land_map.R
+++ R/my_land_map.R
@@ -6,13 +6,16 @@
 #' @param sacel level of map detail, options include small, medium and large
 #' @export
 
-my_land_map <- function(scale = "small",crs = 4326){
+my_land_map <- function(scale = "small",crs = 4326, fill = "grey30", color = "grey70"){
   
   map_df <- rnaturalearth::ne_countries(scale = 'small', returnclass = c("sf")) %>% 
     st_set_crs(4326) %>% 
     st_transform(crs = crs)
   
-  map <- ggplot2::geom_sf(data = map_df)
+  map <- ggplot2::geom_sf(data = map_df,
+                          aes(),
+                          fill = fill,
+                          color = color)
   
   return(map)
 }
